46a4a9d498cf6cac9c1686c287dc8346552c3174
mixinju <151109044+mixinju@users.noreply.github.com>
2026-04-23 19:35:56 +0800

    添加前端工程

---
+++ ui/src/App.tsx
@@ -0,0 +1,19 @@
+import { ConfigProvider, App as AntApp, Typography, Space } from 'antd'
+import { SmileOutlined } from '@ant-design/icons'
+import zhCN from 'antd/locale/zh_CN'
+
+function App() {
+  return (
+    <ConfigProvider locale={zhCN}>
+      <AntApp>
+        <Space direction="vertical" align="center" style={{ width: '100%', paddingTop: 120 }}>
+          <SmileOutlined style={{ fontSize: 48, color: '#1677ff' }} />
+          <Typography.Title level={2}>Skill Eval</Typography.Title>
+          <Typography.Text type="secondary">评测系统前端就绪</Typography.Text>
+        </Space>
+      </AntApp>
+    </ConfigProvider>
+  )
+}
+
+export default App
